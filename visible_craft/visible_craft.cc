12e8f090a41660f26e39da0b22f2fbc89ce415b1
Allan Jones <allan@merinta.com>
2025-11-03 14:46:23 -0600

    Update the TLE data file to reflect only the active mPOWER satellites. Visible craft reports all mPOWER satellites visible in the Clearwater, Florida topocentric view.

--- visible_craft/visible_craft.cc
+++ visible_craft/visible_craft.cc
@@ -1,4 +1,5 @@
 #include <array>
+#include <fstream>
 #include <iostream>
 #include <string>
 #include <vector>
@@ -10,22 +11,56 @@
 #include <SGP4.h>
 #include <TimeSpan.h>
 
-static bool fetch_tle_data(const std::string &tle_filename,
-                           std::array<std::string, 3> &tle_triplet) {
+static size_t fetch_tle_data(const std::string &tle_filename,
+                             std::vector<std::string> &tle_data) {
 
-  if
+  std::ifstream infile(tle_filename);
+  size_t found_craft_count{0};
 
-    return true;
+  if (false == infile.is_open()) {
+    std::cout << "Failed to open TLE File." << std::endl;
+    return 0;
+  }
+
+  std::string line;
+  while (std::getline(infile, line)) {
+    tle_data.push_back(line);
+    found_craft_count++;
+  }
+  found_craft_count = found_craft_count / 3;
+  std::cout << "FOUND (" << found_craft_count << ") craft..." << std::endl;
+
+  infile.close();
+  return found_craft_count;
+}
+
+static bool fetch_tle_triplet(int index,
+                              std::array<std::string, 3> &tle_triplet,
+                              const std::vector<std::string> &tle_data) {
+
+  tle_triplet[0] = tle_data.at(index * 3);
+  tle_triplet[1] = tle_data.at((index * 3) + 1);
+  tle_triplet[2] = tle_data.at((index * 3) + 2);
+
+  return true;
 }
 
 int main() {
   // lat/lon/altitude of PIE airport.
   libsgp4::Observer obs(27.9086, -82.6865, 3.0);
 
-  std::array<std::string, 3> tle_triplet{};
+  std::vector<std::string> tle_data{};
+  size_t craft_count = fetch_tle_data("mPOWER.tle", tle_data);
+  if (craft_count == 0) {
+    return -EXIT_FAILURE;
+  }
+  std::cout << "Found (" << craft_count << ") craft in the TLE file..."
+            << std::endl;
 
-  while (fetch_tle_data("mPOWER.tle", tle_triplet)) {
-    std::string craft_name{};
+  std::array<std::string, 3> tle_triplet;
+  int index{0};
+  while (craft_count-- && fetch_tle_triplet(index++, tle_triplet, tle_data)) {
+    std::string craft_name{tle_triplet[0]};
 
     libsgp4::Tle tle =
         libsgp4::Tle(tle_triplet[0], tle_triplet[1], tle_triplet[2]);
@@ -59,10 +94,9 @@ int main() {
     libsgp4::CoordGeodetic geo = eci.ToGeodetic();
 
     // std::cout << topo << " " << geo << std::endl;
-    if (topo.elevation() <= 10.00) {
-      std::cout << "BELOW HORIZON.";
-    } else {
-      std::cout << "ABOVE HORIZON.";
+    if (topo.elevation() > 10.00) {
+      std::cout << craft_name << " is ABOVE HORIZON: AZ(" << topo.azimuth()
+                << "), EL(" << topo.elevation() << ")" << std::endl;
     }
   }
   return 0;
